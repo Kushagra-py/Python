1e2c70be3b29ae7586d6c6f983fdbaa13fc10287
kushagra <gamerkush213133@gmail.com>
2025-06-26 19:52:03 +0530

    new

---
+++ Case counter for txt.py
@@ -0,0 +1,13 @@
+def ccount():
+    f = open('words.txt')
+    low= 0
+    up = 0
+    data=f.read()
+    for i in data:
+        if i.isupper() and i.isalpha():
+            up += 1
+        elif i.islower() and i.isalpha():
+            low += 1
+    print('uppercase: ',up,"lowercase: ", low)            
+if __name__ == "__main__":
+    ccount()
