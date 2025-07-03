542545ae9e70a6bb1d6676786435136868bb8287
kushagra <gamerkush213133@gmail.com>
2025-07-03 22:52:05 +0530

    name fix

---
+++ C++/ Hello World.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //input output lib
+ 
+int main() { //execution of the program begins from int main()
+  std::cout << "hello world"; //character output
+}
+
+//variable types
+std::string name = "kushcoded"; //part of standard lib
+int level = 1;
+double pi = 3.14; //float
+bool c++ = true;
+char hashtag = "#"; //single char
--- C++/hello.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream> //input output lib
- 
-int main() {
-  std::cout << "hello world"; //character output
-}
