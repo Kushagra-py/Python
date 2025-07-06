b63171d5cbc05e63520b9e6fea45af0ed471a7af
kushcoded <gamerkush213133@gmail.com>
2025-07-06 20:38:19 +0530

    Merge

--- C++/Hello_World.cpp
+++ C++/Hello_World.cpp
@@ -1,5 +1,12 @@
 #include <iostream> //input output lib
+
+
+//define function
+void function() {
+  std::cout << "Bye World!" << std::endl;
+}
  
+
 int main() { //execution of the program begins from int main()
   
   std::cout << "Hello world!\n"; //cout = character output
@@ -7,14 +14,13 @@ int main() { //execution of the program begins from int main()
   std::string username;
   std::cout << "Enter your name: ";
   std::cin >> username; //char input (var must be declared)
-  std::cout << "Hello Master " << username << "!"; //chaining
+  std::cout << "Hello Master " << username << "!" << std::endl; //chaining
 
-  return 0;
-}
+
 
 //variable types
 std::string name = "kushcoded"; //part of standard lib
-int level = 1;
+int File_number = 1;
 double pi = 3.14; //float
 bool cpp = true;
 char grade  = 'A'; //single char, uses 'single quote'
@@ -33,3 +39,40 @@ logical operators:
 || or
 ! not
 */
+
+//conditionals
+  int power;
+  
+  std::cout << "Enter power level: ";
+  std::cin >> power;
+  
+  if (power > 9000) {
+    std::cout << "IT'S OVER 9000!" << std::endl; // \n + std::flush = std::endl
+  }
+  else if (power == 9000) {
+    std::cout << "Almost there..." << std::endl;
+  }
+  else {
+    std::cout << "You are weak!" << std::endl;
+  }
+  
+
+//for loop
+for(int i = 0; i < 10; i++) {
+    std::cout << i << std::endl;
+}
+std::cout << "warmup complete" << std::endl;
+
+//while loop
+int countdown;
+std::cout << "Countdown from: ";
+std::cin >> countdown;
+while(countdown != 0) {
+    std::cout << countdown << std::endl;
+    countdown--;
+}
+
+function(); //function call
+
+return 0;
+}
